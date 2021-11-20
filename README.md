898b0b01b211472a47e4ff97bb39b6fcded3b253
Shayan Yousefi <yusefshayan@gmail.com>
2021-11-20 13:39:11 +0330

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+#How To Use
+
+run `npm run load-test`.
+
+#command line options:
+
+option name | alias | desc.
+----| ------ | ----
+address| a | url of socket server
+max_client | mc | maximum number of client
+polling_percentage | pp | number of client who use tcp polling only
+client_creation_interval | cci | interval between each client creation
+emit_message_interval | emi | interval between each message sent
---
+++ load-test.ts
@@ -0,0 +1,85 @@
+import {io} from "socket.io-client";
+import yargs from 'yargs';
+
+yargs(process.argv.slice(2)).command('*', '', {
+    'address': {
+        alias:'a',
+        type: 'string'
+    },
+    'max_client': {
+        alias: 'mc',
+        type: 'number',
+    },
+    'polling_percentage': {
+        alias: 'pp',
+        type: 'number',
+    },
+    'client_creation_interval': {
+        alias: 'cci',
+        type: 'number',
+    },
+    'emit_message_interval': {
+        alias: 'emi',
+        type: 'number'
+    }
+}, argv => {
+
+    const URL = argv.address as string;
+    const MAX_CLIENTS = argv.max_client as number;
+    const POLLING_PERCENTAGE = argv.polling_percentage as number;
+    const CLIENT_CREATION_INTERVAL_IN_MS = argv.client_creation_interval;
+    const EMIT_INTERVAL_IN_MS = argv.emit_message_interval;
+
+    let clientCount = 0;
+    let lastReport = new Date().getTime();
+    let packetsSinceLastReport = 0;
+
+    const createClient = () => {
+        // for demonstration purposes, some clients stay stuck in HTTP long-polling
+        const transports =
+            Math.random() < POLLING_PERCENTAGE ? ["polling"] : ["polling", "websocket"];
+
+        const socket = io(URL, {
+            transports,
+        });
+
+        setInterval(() => {
+            socket.emit("clientMessage",{
+                message:'new event',
+                socket:socket.id
+            });
+        }, EMIT_INTERVAL_IN_MS);
+
+        socket.on("serverMessage", (msg) => {
+            packetsSinceLastReport++;
+        });
+
+        socket.on("disconnect", (reason) => {
+            console.log(`disconnect due to ${reason}`);
+        });
+
+
+        if (++clientCount < MAX_CLIENTS) {
+            setTimeout(createClient, CLIENT_CREATION_INTERVAL_IN_MS);
+        }
+    };
+
+    createClient();
+
+    const printReport = () => {
+        const now = new Date().getTime();
+        const durationSinceLastReport = (now - lastReport) / 1000;
+        const packetsPerSeconds = (
+            packetsSinceLastReport / durationSinceLastReport
+        ).toFixed(2);
+
+        console.log(
+            `[${(new Date()).toTimeString()}] client count: ${clientCount} ; average packets received per second: ${packetsPerSeconds}`
+        );
+
+        packetsSinceLastReport = 0;
+        lastReport = now;
+    };
+
+    setInterval(printReport, 5000);
+}).argv
